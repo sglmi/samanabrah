7dad9e621a06d501832a00414927166520b45e4a
Saeid Gholami <saeidxgholami@gmail.com>
2021-10-26 18:03:45 +0330

    Display error message on login form.

--- samanabrah/users/views.py
+++ samanabrah/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def index(request):
@@ -8,13 +9,15 @@ def index(request):
 
 
 def user_login(request):
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect("users:succ")
-    else:
-        print("Bad Credential.")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("users:succ")
+        else:
+            messages.error(request, "نام کاربری یا رمز ورود اشتباه است.")
+            print("Bad Credential.")
 
     return render(request, "users/login.html")
